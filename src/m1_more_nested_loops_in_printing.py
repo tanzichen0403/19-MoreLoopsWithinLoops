b90618bf3e46844f24e3c61af6bbe01689729224
Tanz <tanz@rose-hulman.edu>
2019-01-21 13:21:15 -0500

    1done

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -4,8 +4,8 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zichen Tan.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -56,7 +56,7 @@ def triangle_right_justified(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: Do the following problem FIRST, then convert x's to spaces:
@@ -74,7 +74,12 @@ def triangle_right_justified(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
-
+    for k in range (r):
+        for i in range (r-k-1):
+            print(" ",end="")
+        for m in range(1,k+2):
+            print (m,end="")
+        print ("")
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -110,7 +115,7 @@ def triangle_upside_down(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -118,7 +123,12 @@ def triangle_upside_down(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
-
+    for k in range(r):
+        for i in range(k):
+            print(" ", end="")
+        for m in range(1,r-k+1):
+            print(m, end="")
+        print("")
 
 def run_test_vee():
     """ Tests the    vee    function. """
@@ -161,7 +171,7 @@ def vee(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -169,6 +179,15 @@ def vee(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for k in range (r):
+        for i in range(k):
+            print(" ", end="")
+        for m in range(1,r-k+1):
+            print(m, end="")
+        print("-", end="")
+        for i in range(r-k,0,-1):
+            print(i,end="")
+        print("")
 
 
 def run_test_numbers_constant_forward():
@@ -217,7 +236,7 @@ def numbers_constant_forward(r, maxnum, n):
     Preconditions:  r, maxnum and n are positive integers.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # done: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: What loop structure do you need for this problem?
@@ -227,6 +246,12 @@ def numbers_constant_forward(r, maxnum, n):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for i in range (r):
+        for k in range(1,maxnum+1):
+            for m in range (n):
+                print (k,end="")
+            print(" ",end="")
+        print(" ")
 
 
 def run_test_numbers_constant_backwards():
@@ -261,7 +286,7 @@ def numbers_constant_backwards(r, maxnum, n):
     Preconditions:  r, maxnum and n are positive integers.
     """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DOne: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -269,7 +294,12 @@ def numbers_constant_backwards(r, maxnum, n):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
-
+    for i in range (r):
+        for k in range(maxnum,0,-1):
+            for m in range (n):
+                print (k,end="")
+            print(" ",end="")
+        print(" ")
 
 def run_test_numbers_increasing_forward():
     """ Tests the    numbers_increasing_forward    function. """
@@ -306,6 +336,8 @@ def numbers_increasing_forward(r, maxnum):
        1 22 333
        1 22 333
 
+
+
     Another example, when r = 2 and maxnum = 7:
        1 22 333 4444 55555 666666 7777777
        1 22 333 4444 55555 666666 7777777
@@ -321,6 +353,12 @@ def numbers_increasing_forward(r, maxnum):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for i in range(r):
+        for k in range(1,maxnum+1):
+            for m in range (k):
+                print (k,end="")
+            print(" ",end="")
+        print (" ")
 
 
 # -----------------------------------------------------------------------------
